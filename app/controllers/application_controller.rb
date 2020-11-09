f7e20c8d57108b5412c203a6e721681faae650c2
kanekodaichi <knkdic.bfc@gmail.com>
2020-11-09 18:20:09 +0900

    Userモデルにプロフィールを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,17 +14,18 @@ class ApplicationController < ActionController::Base
     { locale: I18n.locale }.merge options
   end
 
-  def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(_resource)
     books_path
   end
 
-  def after_sign_out_path_for(resource)
+  def after_sign_out_path_for(_resource)
     new_user_session_path
   end
-  
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[username profile])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'users/show' => 'users#show'
   devise_for :users
+  resources :users, only: [:show]
   scope '(:locale)' do
     resources :books
   end
-  root to: "books#index"
+  root to: 'books#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
